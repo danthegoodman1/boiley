3f6684a9f373892f242244975fe5f256ee9ca371
BuildTools <unconfigured@null.spigotmc.org>
2019-08-19 15:33:49 -0400

    init

---
+++ bin/boiley.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+require('yargs') // eslint-disable-line
+  .command('serve [port]', 'start the server', (yargs) => {
+    yargs
+      .positional('port', {
+        describe: 'port to bind on',
+        default: 5000
+      })
+  }, (argv) => {
+    if (argv.verbose) console.info(`start server on :${argv.port}`)
+  })
+  .option('verbose', {
+    alias: 'v',
+  })
+  .argv
